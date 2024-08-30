2ce5eebfa6dd6ca78963655017b4daa96c759bf3
Ozge Cetin <ozgecetin43@gmail.com>
2024-08-30 22:52:31 +0300

    trial expires component was changed

--- src/components/TrialExpired/index.tsx
+++ src/components/TrialExpired/index.tsx
@@ -17,6 +17,25 @@ export const TrialExpired: React.FC = () => {
       <div className="trial-expired-container-content">
         <div className="trial-expaired-content-left">
           <img src={logo} alt="logo" />
+          <h3>Clock is ticking!</h3>
+          <p>
+            We hope you were able to spend the last 25 days exploring how
+            Dashboard can help you save time and increase your productivity!
+          </p>
+          <p>
+            <b>Your Free trial will end in 20 days.</b> When your trial expires,
+            you'll revert to our Free plan. If you have Dashboard that use paid
+            features, they will be paused.
+          </p>
+          <p>Paid features include:</p>
+          <ul>
+            <li>Unlimited access acros devices</li>
+            <li>24/7 account monitoring</li>
+            <li>One-to-many sharing</li>
+          </ul>
+          <p>
+            To keep using our paid features, upgrade to one of our paid plans.
+          </p>
           <p>
             Upgrade your plan from a <b>Free trial</b> to <b>Premium Plan</b>
           </p>
